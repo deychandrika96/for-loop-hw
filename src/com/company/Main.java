ca0bad1cd4fa3d0b7a33ef58a72b1efc80fc4517
Chandrika <sejutispr.dey01@gmail.com>
2020-10-05 21:05:36 -0400

    for-loop hw

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//find the numbers between 50 - 100
+
+
+        for (int x = 50; x < 100; x++) {
+            System.out.println("Value of x :" + x);
+        }
+
+
+    }
+}
